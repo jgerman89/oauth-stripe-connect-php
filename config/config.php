cf4747bc5ed7354b8d1e069ae281d88c9e31607c
Jordan German <jordan.german@salonjg.com>
2018-08-09 07:47:26 -0400

    Update config.php

--- config/config.php
+++ config/config.php
@@ -2,9 +2,9 @@
 
 require_once('../vendor/autoload.php');
 
-define("CLIENT_ID", "ca_your_client_id"); // Your client ID: https://dashboard.stripe.com/account/applications/settings
-define("SECRET_KEY","sk_your_secret_key"); // Your secret API KEY: https://dashboard.stripe.com/account/apikeys
+define("CLIENT_ID", "ca_9O9Rw5orFobhEjUbDJRVfSJOd8u5jqvK"); // Your client ID: https://dashboard.stripe.com/account/applications/settings
+define("SECRET_KEY","sk_live_oc46bVLC4bFTC0WulfZsbcdi"); // Your secret API KEY: https://dashboard.stripe.com/account/apikeys
 define("REDIRECT_URL", "http://12.12.12.12/connected.php"); // https://dashboard.stripe.com/account/applications/settings
 
-\Stripe\Stripe::setApiKey(SECRET_KEY);
+\Stripe\Stripe::setApiKey(sk_live_oc46bVLC4bFTC0WulfZsbcdi);
 ?>
